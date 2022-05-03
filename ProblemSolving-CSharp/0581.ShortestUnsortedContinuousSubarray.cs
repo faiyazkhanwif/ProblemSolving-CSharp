18598f71109d08c4a292078cf39326cd39a70b4a
faiyazkhanwif <faiyazkhanwif@gmail.com>
2022-05-04 00:51:45 +0800

    Medium

---
+++ ProblemSolving-CSharp/0581.ShortestUnsortedContinuousSubarray.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    public class _0581
+    {
+        public int FindUnsortedSubarray(int[] nums)
+        {
+            int[] myarr = new int[nums.Length];
+            nums.CopyTo(myarr, 0);
+
+            Array.Sort(myarr);
+
+            //for (int i = 0; i < myarr.Length; i++)
+            //{
+            //    Console.WriteLine(myarr[i]);
+            //}
+            int start = 0;
+            int end = nums.Length - 1;
+            bool needsorting = false;
+
+            for (int i = 0; i < myarr.Length; i++)
+            {
+                if (myarr[i] != nums[i])
+                {
+                    needsorting = true;
+                    start = i;
+                    break;
+                }
+            }
+            if (!needsorting)
+            {
+                return 0;
+            }
+            //Console.WriteLine("start - "+ start);
+
+            for (int i = myarr.Length - 1; i >= 0; i--)
+            {
+                if (myarr[i] != nums[i])
+                {
+                    end = i;
+                    break;
+                }
+            }
+            //Console.WriteLine("end - " + end);
+            return end - start + 1;
+        }
+    }
+}
--- ProblemSolving-CSharp/Program.cs
+++ ProblemSolving-CSharp/Program.cs
@@ -1,36 +1,9 @@
-﻿ string SortSentence(string s)
+﻿int test(int[] nums)
 {
-    string[] words = s.Split(' ');
-    string res = "";
 
-    Dictionary<int, string> result = new Dictionary<int, string>();
-
-    for (int i = 0; i < words.Length; i++)
-    {
-        string tempword = words[i];
-        char ind = tempword[tempword.Length - 1];
-
-        string word = words[i].Remove(words[i].Length - 1);
-
-        Console.WriteLine(ind + "---" + word);
-
-        int intind = ind - '0';
-        result.Add(intind, word);
-    }
-
-    for (int i = 1; i <= words.Length; i++)
-    {
-        res += result[i];
-        if (i != words.Length)
-        {
-
-            res += " ";
-        }
-    }
-
-    return res;
+    return 0;
 }
 
+int[] arr = {2,1 };
 
-
-Console.WriteLine(SortSentence("is2 sentence4 This1 a3"));
+Console.WriteLine(test(arr));
