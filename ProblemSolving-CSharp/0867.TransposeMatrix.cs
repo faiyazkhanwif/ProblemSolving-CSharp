3a562ab9bec03dba57681b98c3cf9e34a499983a
faiyazkhanwif <faiyazkhanwif@gmail.com>
2022-06-07 02:11:24 +0800

    Easy

---
+++ ProblemSolving-CSharp/0867.TransposeMatrix.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    public class _0867
+    {
+        public int[][] Transpose(int[][] matrix)
+        {
+            int rowlen = matrix[0].Length;
+            int collen = matrix.Length;
+            int[][] ans = new int[rowlen][];
+            for (int i = 0; i < rowlen; i++)
+            {
+                ans[i] = new int[collen];
+            }
+            
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    ans[j][i] = matrix[i][j];
+                }
+            }
+            return ans;
+            
+        }
+    }
+}
---
+++ ProblemSolving-CSharp/1480.RunningSumof1dArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    public class _1480
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            
+            int sum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = nums[i]+sum;
+                sum = nums[i];
+            }
+            return nums;
+
+        }
+    }
+}
