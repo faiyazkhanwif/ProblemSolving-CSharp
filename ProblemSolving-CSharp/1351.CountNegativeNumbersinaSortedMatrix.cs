ca59d43ca0a20b3487c412df9e14804aa99de180
faiyazkhanwif <faiyazkhanwif@gmail.com>
2022-06-12 15:26:56 +0800

    Easy

---
+++ ProblemSolving-CSharp/1351.CountNegativeNumbersinaSortedMatrix.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    internal class _1351
+    {
+        public int CountNegatives(int[][] grid)
+        {
+            int m = grid.Length;
+            int count = 0;
+
+            for (int i = 0; i < m; i++)
+            {
+                int tempN = grid[i].Length - 1;
+                while (tempN >= 0 && grid[i][tempN] < 0)
+                {
+                    //Console.WriteLine("Val: "+grid[i][tempN]);
+                    count++;
+                    tempN--;
+                }
+
+            }
+
+            return count;
+        }
+    }
+}
---
+++ ProblemSolving-CSharp/2089.FindTargetIndicesAfterSortingArray.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    public class _2089
+    {
+        public IList<int> TargetIndices(int[] nums, int target)
+        {
+            List<int> result = new List<int>();
+            Array.Sort(nums);
+
+            //for (int q = 0; q < nums.Length; q++)
+            //{
+            //    Console.Write(nums[q] + " ");
+            //}
+            //Console.WriteLine();
+
+            int i = 0;
+            int j = nums.Length - 1;
+            bool found = false;
+            int matchedInd = -1;
+
+            while (i <= j)
+            {
+                int mid = i + (j - i) / 2;
+
+                //Console.WriteLine("mid: " + mid);
+                //Console.WriteLine("i: " + i);
+                //Console.WriteLine("j: " + j);
+                //Console.WriteLine(nums[mid]);
+                if (nums[mid] != target)
+                {
+                    if (nums[mid] < target)
+                    {
+                        i = mid + 1;
+                    }
+                    else
+                    {
+                        j = mid - 1;
+                    }
+                }
+                else
+                {
+                    matchedInd = mid;
+                    result.Add(matchedInd);
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                return result;
+            }
+
+            bool existsInBack = true;
+            int tempindB = matchedInd - 1;
+            while (existsInBack)
+            {
+                if (tempindB < 0)
+                {
+                    existsInBack = false;
+                    break;
+                }
+                if (nums[tempindB] == target)
+                {
+                    result.Add(tempindB);
+                    tempindB--;
+                }
+                else
+                {
+                    existsInBack = false;
+                }
+            }
+
+            bool existsInFront = true;
+            int tempindF = matchedInd + 1;
+
+            while (existsInFront)
+            {
+                if (tempindF >= nums.Length)
+                {
+                    existsInFront = false;
+                    break;
+                }
+                if (nums[tempindF] == target)
+                {
+                    result.Add(tempindF);
+                    tempindF++;
+                }
+                else
+                {
+                    existsInFront = false;
+                    break;
+                }
+            }
+
+            result.Sort();
+            return result;
+        }
+    }
+}
