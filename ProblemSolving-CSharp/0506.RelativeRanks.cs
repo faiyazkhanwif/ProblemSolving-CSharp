600ed8f2a2453ad621ca6f39d0b10dad85ac5dc6
faiyazkhanwif <faiyazkhanwif@gmail.com>
2022-07-20 03:12:46 +0800

    Easy

---
+++ ProblemSolving-CSharp/0506.RelativeRanks.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    internal class _0506
+    {
+        public string[] FindRelativeRanks(int[] score)
+        {
+            int[] scoreSorted = new int[score.Length];
+            string[] ans = new string[score.Length];
+            score.CopyTo(scoreSorted, 0);
+
+            Array.Sort(scoreSorted);
+            Array.Reverse(scoreSorted);
+            Dictionary<int, string> scoreMap = new Dictionary<int, string>();
+
+            for (int i = 0; i < scoreSorted.Length; i++)
+            { 
+                string val = "";
+                if (i==0)
+                {
+                    val = "Gold Medal";
+                }else if (i == 1)
+                {
+                    val = "Silver Medal";
+                }else if(i == 2)
+                {
+                    val = "Bronze Medal";
+                }
+                else
+                {
+                    val = (i + 1).ToString();
+                }
+                scoreMap.Add(scoreSorted[i], val);
+            }
+
+            for (int i = 0; i < score.Length; i++)
+            {
+                ans[i] = scoreMap[score[i]];
+            }
+            return ans;
+        }
+    }
+}
---
+++ ProblemSolving-CSharp/2210.CountHillsandValleysinanArray.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    internal class _2210
+    {
+        public int CountHillValley(int[] nums)
+        {
+            int cnt = 0;
+            int i = 0;
+            while (i < nums.Length)
+            {
+                if (i > 0 && i < nums.Length - 1)
+                {
+                    if (nums[i - 1] < nums[i] && nums[i] > nums[i + 1])
+                    {
+                        cnt++;
+                        i++;
+                        continue;
+                    }
+                    if (nums[i - 1] > nums[i] && nums[i] < nums[i + 1])
+                    {
+                        cnt++;
+                        i++;
+                        continue;
+                    }
+                    if (nums[i - 1] == nums[i])
+                    {
+                        int ind = i;
+                        while (ind < nums.Length - 1 && nums[ind] == nums[i - 1])
+                        {
+                            ind++;
+                        }
+                        if (i - 2 >= 0 && nums[i - 2] < nums[i] && nums[i] > nums[ind])
+                        {
+                            cnt++;
+                            i = ind;
+                            continue;
+                        }
+                        if (i - 2 >= 0 && nums[i - 2] > nums[i] && nums[i] < nums[ind])
+                        {
+                            cnt++;
+                            i = ind;
+                            continue;
+                        }
+                    }
+
+                    i++;
+                    continue;
+
+                }
+
+                i++;
+
+            }
+            return cnt;
+
+        }
+    }
+}
