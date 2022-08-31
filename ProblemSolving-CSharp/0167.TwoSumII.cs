9ebb4f88399f97b37fd1b76ab9c397e3e7b1d593
faiyazkhanwif <faiyazkhanwif@gmail.com>
2022-09-01 06:02:31 +0800

    Medium

---
+++ ProblemSolving-CSharp/0167.TwoSumII.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    internal class _0167
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            int[] res = new int[2];
+            int p = 0;
+            int q = numbers.Length - 1;
+
+            while (p < q)
+            {
+                int sum = numbers[p]+numbers[q];
+                if (sum>target)
+                {
+                    q--;
+                }else if (sum < target)
+                {
+                    p++;
+                }
+                else
+                {
+                    res[0] = p+1;
+                    res[1] = q+1;
+                    break;
+                }
+            }
+            return res;
+        }
+    }
+}
