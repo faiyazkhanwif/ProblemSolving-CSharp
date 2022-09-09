b8da860c62fe5349610816154e4a93f6e8998187
faiyazkhanwif <faiyazkhanwif@gmail.com>
2022-09-10 01:57:09 +0800

    Medium

---
+++ ProblemSolving-CSharp/0075.SortColors.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    internal class _0075
+    {
+        public void SortColors(int[] nums)
+        {
+            int max = int.MinValue; 
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i]>max)
+                {
+                    max = nums[i];
+                }
+            }
+
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i]))
+                {
+                    dic[nums[i]] = dic[nums[i]] + 1;
+                }
+                else
+                {
+                    dic.Add(nums[i], 1);
+                }
+            }
+
+            int ind = 0;
+
+            for (int i = 0; i <= max; i++)
+            {
+                if (dic.ContainsKey(i))
+                {
+                    int count = dic[i];
+                    for (int p = 0; p < count; p++)
+                    {
+                        nums[ind] = i;
+                        ind++;
+                    }
+
+                }
+            }
+
+        }
+    }
+}
