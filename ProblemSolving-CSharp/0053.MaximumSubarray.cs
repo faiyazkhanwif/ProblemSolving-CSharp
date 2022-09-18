6660d79b0eb0bd1c6d40e3568e6f5f92f6a1de1f
faiyazkhanwif <faiyazkhanwif@gmail.com>
2022-09-19 00:37:57 +0800

    Medium

---
+++ ProblemSolving-CSharp/0053.MaximumSubarray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    internal class _0053
+    {
+        //O(n^3) finding out all possible subarrays - passes 200 / 209
+
+        public int MaxSubArray(int[] nums)
+        {
+            int maxSize = int.MinValue;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i; j < nums.Length; j++)
+                {
+                    int tempSize = 0;
+
+                    for (int k = i; k <= j; k++)
+                    {
+                        tempSize+=nums[k];
+                    }
+
+                    if (tempSize>maxSize)
+                    {
+                        maxSize = tempSize;
+                    }
+                }
+            }
+
+            return maxSize;
+
+        }
+    }
+}
