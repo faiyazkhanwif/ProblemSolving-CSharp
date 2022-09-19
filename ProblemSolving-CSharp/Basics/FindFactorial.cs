f9d5b48dbaf46547dff35342cb5264ccbb1cc489
faiyazkhanwif <faiyazkhanwif@gmail.com>
2022-09-20 07:55:59 +0800

    Easy

---
+++ ProblemSolving-CSharp/Basics/FindFactorial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp.Basics
+{
+    internal class FindFactorial
+    {
+        public int FindFactorialRecursively(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            return n * FindFactorialRecursively(n - 1);
+        }
+
+        public int FindFactorialIteratively(int n)
+        {
+            int ans = n;
+            n = n - 1;
+            while (n > 0)
+            {
+                ans = ans * (n);
+                n--;
+            }
+            return ans;
+        }
+    }
+
+}
