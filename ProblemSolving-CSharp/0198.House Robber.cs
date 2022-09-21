aad53ae89af084ec28aed5282193d23cee666c81
faiyazkhanwif <faiyazkhanwif@gmail.com>
2022-09-22 00:58:21 +0800

    Medium

---
+++ ProblemSolving-CSharp/0198.House Robber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    internal class _0198
+    {
+        //O(n)
+        public int Rob(int[] nums)
+        {
+
+            if (nums == null || nums.Length == 0)
+            {
+                return 0;
+            }
+
+            if (nums.Length == 1)
+            {
+                return nums[0];
+            }
+
+            int[] dp = new int[nums.Length];
+
+            dp[0] = nums[0];
+            dp[1] = Math.Max(dp[0], nums[1]);
+
+
+            for (int i = 2; i < nums.Length; i++)
+            {
+                dp[i] = Math.Max(dp[i - 1], dp[i - 2] + nums[i]);
+            }
+
+            return dp[dp.Length - 1];
+        }
+    }
+}
