14a4b2825173b1e1587083416f8a98733aa9b1df
faiyazkhanwif <faiyazkhanwif@gmail.com>
2022-09-24 11:44:36 +0800

    Easy

---
+++ ProblemSolving-CSharp/0896.MonotonicArray.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    internal class _0896
+    {   //O(n)
+        public bool IsMonotonic(int[] nums)
+        {
+            if (nums.Length == 1)
+            {
+                return true;
+            }
+            bool increasing = false;
+            bool decreasing = false;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (increasing && decreasing)
+                {
+                    return false;
+                }
+
+                if (nums[i] < nums[i - 1])
+                {
+                    decreasing = true;
+                }
+
+                if (nums[i] > nums[i - 1])
+                {
+                    increasing = true;
+                }
+            }
+
+            if (increasing && decreasing)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
