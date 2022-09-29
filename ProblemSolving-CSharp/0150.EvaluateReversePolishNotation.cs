7565cdaa5c80b4d03233859527e6cf1901ba806c
faiyazkhanwif <faiyazkhanwif@gmail.com>
2022-09-29 11:00:31 +0800

    Medium

---
+++ ProblemSolving-CSharp/0150.EvaluateReversePolishNotation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    internal class _0150
+    {
+        //O(n)
+        public int EvalRPN(string[] tokens)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                if (tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/")
+                {
+                    int elementSecond = stack.Pop();
+                    int elementFirst = stack.Pop();
+                    if (tokens[i] == "+")
+                    {
+                        int res = elementFirst + elementSecond;
+                        stack.Push(res);
+                    }
+                    else if (tokens[i] == "-")
+                    {
+                        int res = elementFirst - elementSecond;
+                        stack.Push(res);
+                    }
+                    else if (tokens[i] == "*")
+                    {
+                        int res = elementFirst * elementSecond;
+                        stack.Push(res);
+                    }
+                    else if (tokens[i] == "/")
+                    {
+                        int res = elementFirst / elementSecond;
+                        stack.Push(res);
+                    }
+                }
+                else
+                {
+                    stack.Push(int.Parse(tokens[i]));
+                }
+            }
+
+            return stack.Pop();
+        }
+    }
+}
