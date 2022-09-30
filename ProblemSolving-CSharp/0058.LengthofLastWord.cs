0411c0cb9868c70b6c245acd4a6242d90d858dbd
faiyazkhanwif <faiyazkhanwif@gmail.com>
2022-09-30 13:19:55 +0800

    Easy

---
+++ ProblemSolving-CSharp/0058.LengthofLastWord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    internal class _0058
+    {
+        public int LengthOfLastWord(string s)
+        {
+            s = s.Trim();
+            string[] words = s.Split(" ").ToArray();
+            return words[words.Length - 1].Length;
+        }
+    }
+}
