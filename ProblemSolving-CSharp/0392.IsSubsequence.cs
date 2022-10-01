d92b7085fe1f1d3c3539cdc114064f73e3615b83
faiyazkhanwif <faiyazkhanwif@gmail.com>
2022-10-02 06:58:25 +0800

    Easy

---
+++ ProblemSolving-CSharp/0392.IsSubsequence.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    internal class _0392
+    {
+        //O(n)
+        public bool IsSubsequence(string s, string t)
+        {
+            int ti = t.Length - 1;
+            int si = s.Length - 1;
+
+            while (si >= 0 && ti >= 0)
+            {
+                if (s[si] == t[ti])
+                {
+                    si--;
+                }
+
+                //Console.WriteLine("si: "+si);
+                ti--;
+            }
+
+            return si < 0;
+        }
+    }
+}
