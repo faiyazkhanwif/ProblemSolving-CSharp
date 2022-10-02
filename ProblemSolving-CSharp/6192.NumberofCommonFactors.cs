8c2701e7c0ce9e84fef79f6ff471565ad35fd0f8
faiyazkhanwif <faiyazkhanwif@gmail.com>
2022-10-02 12:00:41 +0800

    Easy

---
+++ ProblemSolving-CSharp/6192.NumberofCommonFactors.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    internal class _6192
+    {
+        //O(n)
+        public int CommonFactors(int a, int b)
+        {
+            int max = Math.Max(a,b);
+            int count = 0;
+
+            for (int i = 1; i <= max; i++)
+            {
+                if (a%i==0 && b%i==0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ ProblemSolving-CSharp/6193.MaximumSumofanHourglass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    internal class _6193
+    {
+        public int MaxSum(int[][] grid)
+        {
+            int m = grid.Length;
+            int n = grid[0].Length;
+            if (m < 3 || n < 3)
+            {
+                return 0;
+            }
+
+            int maxSum = int.MinValue;
+
+            for (int i = 0; i < m - 2; i++)
+            {
+                for (int j = 0; j < n - 2; j++)
+                {
+                    int sum = (grid[i][j] + grid[i][j + 1] + grid[i][j + 2])
+                                + (grid[i + 1][j + 1])
+                                + (grid[i + 2][j] + grid[i + 2][j + 1] + grid[i + 2][j + 2]);
+                    maxSum = Math.Max(maxSum, sum);
+                }
+            }
+
+            return maxSum;
+        }
+    }
+}
