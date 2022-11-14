413f7d6fd7228208ab63338a5bc5379a5c757a51
faiyazkhanwif <faiyazkhanwif@gmail.com>
2022-11-14 13:00:03 +0800

    Easy

---
+++ ProblemSolving-CSharp/0242.Valid Anagram.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    internal class _0242
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            Dictionary<char, int> freqDic = new Dictionary<char, int>();
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (freqDic.ContainsKey(t[i]))
+                {
+                    freqDic[t[i]]++;
+                }
+                else
+                {
+                    freqDic.Add(t[i], 1);
+                }
+            } 
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (freqDic.ContainsKey(s[i]) && freqDic[s[i]]>0)
+                {
+                    freqDic[s[i]]--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
