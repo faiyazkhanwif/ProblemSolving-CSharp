e6c52f20e9358ade3f3d484b280dc81f033d07c9
faiyazkhanwif <faiyazkhanwif@gmail.com>
2022-11-14 17:24:20 +0800

    Easy

---
+++ ProblemSolving-CSharp/0125.ValidPalindrome.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    internal class _0125
+    {
+        public bool isPalindrome(string s)
+        {
+            s = s.ToLower().Replace("[^a-zA-Z0-9 -]", "");
+            int i = 0;
+            int j = s.Length - 1;
+            while (i <= j)
+            {
+                if (s[i] != s[j])
+                {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+            return true;
+        }
+    }
+}
