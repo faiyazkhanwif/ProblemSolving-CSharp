0173df4566039a8d47c03ed58a7b68a00bea76b4
faiyazkhanwif <faiyazkhanwif@gmail.com>
2022-12-17 19:47:22 +0800

    Medium

---
+++ ProblemSolving-CSharp/0462.MinimumMovestoEqualArrayElementsII.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    internal class _0462
+    {
+        public int MinMoves2(int[] nums)
+        {
+            int moves = 0;
+
+            Array.Sort(nums);
+            int median = nums[nums.Length / 2];
+
+            foreach (int num in nums)
+            {
+                moves += Math.Abs(median - num);
+            }
+            return moves;
+        }
+    }
+}
