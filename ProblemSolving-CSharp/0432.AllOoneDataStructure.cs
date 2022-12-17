960f71842fff7fcd8566d403c54cd98ccdea455a
faiyazkhanwif <faiyazkhanwif@gmail.com>
2022-12-17 19:12:42 +0800

    Hard

---
+++ ProblemSolving-CSharp/0432.AllOoneDataStructure.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    internal class _0432
+    {
+        //TLE
+        public class AllOne
+        {
+            Dictionary<string, int> map; 
+
+            public AllOne()
+            {
+                map = new Dictionary<string, int>(); 
+            }
+
+            public void Inc(string key)
+            {
+                if (map.ContainsKey(key))
+                {
+                    map[key]++;
+                }
+                else
+                {
+                    map.Add(key, 1);
+                }
+            }
+
+            public void Dec(string key)
+            {
+                if (map.ContainsKey(key))
+                {
+                    map[key]--;
+                    if (map[key] <= 0)
+                    {
+                        map.Remove(key);
+                    }
+                }
+            }
+
+            public string GetMaxKey()
+            {
+                if (map.Count != 0)
+                {
+                    return map.MaxBy(keyValuePair => keyValuePair.Value).Key;
+                }
+                else
+                {
+                    return "";
+                }
+            }
+
+            public string GetMinKey()
+            {
+                if (map.Count != 0)
+                {
+                    return map.MinBy(keyValuePair => keyValuePair.Value).Key;
+                }
+                else
+                {
+                    return "";
+                }
+            }
+        }
+
+    }
+}
