4e331a63cca2eba359c51133149e7567117d9969
faiyazkhanwif <faiyazkhanwif@gmail.com>
2022-12-24 22:34:54 +0800

    Easy

---
+++ ProblemSolving-CSharp/0704.BinarySearch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    internal class _0704
+    {
+        public int Search(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+                else
+                {
+                    if (nums[mid] > target)
+                    {
+                        right = mid - 1;
+                    }
+                    else
+                    {
+                        left = mid + 1;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
