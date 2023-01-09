d55cdc550bc240b06efa30fe7fc5609c383dd563
faiyazkhanwif <faiyazkhanwif@gmail.com>
2023-01-09 20:51:33 +0800

    Medium

---
+++ ProblemSolving-CSharp/0153.FindMinimuminRotatedSortedArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    internal class _0153
+    {
+        public int FindMin(int[] nums)
+        {
+            if (nums[nums.Length - 1] > nums[0])
+            {
+                return nums[0];
+            }
+
+            int startInd = 0;
+            int EndInd = nums.Length - 1;
+
+            while (startInd < EndInd)
+            {
+                int midInd = startInd + (EndInd - startInd) / 2;
+                if (nums[midInd] < nums[nums.Length - 1])
+                {
+                    EndInd = midInd;
+                }
+                else
+                {
+                    startInd = midInd + 1;
+                }
+            }
+            return nums[startInd];
+        }
+
+    }
+}
