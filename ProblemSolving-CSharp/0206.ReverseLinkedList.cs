13009abf08a184ffd7f19ee68fba061260fd24ef
faiyazkhanwif <faiyazkhanwif@gmail.com>
2023-02-13 22:40:52 +0800

    Easy

---
+++ ProblemSolving-CSharp/0206.ReverseLinkedList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    internal class _0206
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public class Solution
+        {
+            //Iterative
+            public ListNode ReverseList(ListNode head)
+            {
+                if (head == null)
+                {
+                    return null;
+                }
+                if (head.next == null)
+                {
+                    return head;
+                }
+
+                ListNode prev = null;
+                ListNode curr = head;
+
+                while (curr != null)
+                {
+                    ListNode nextNode = curr.next;
+                    curr.next = prev;
+                    prev = curr;
+                    curr = nextNode;
+                }
+
+                return prev;
+
+            }
+        }
+    }
+}
