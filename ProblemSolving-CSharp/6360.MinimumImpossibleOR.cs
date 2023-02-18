9dbe44b111d216b43c2f7d80d025dc793cf8d606
faiyazkhanwif <faiyazkhanwif@gmail.com>
2023-02-19 00:08:25 +0800

    Medium

---
+++ ProblemSolving-CSharp/6360.MinimumImpossibleOR.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp
+{
+    internal class _6360
+    {
+        public int MinImpossibleOR(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>();
+
+            // Find all possible bitwise OR of the possible subsequences
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int result = nums[i];
+                set.Add(result);
+
+                foreach (int val in set.ToList())
+                {
+                    result = val | nums[i];
+                    set.Add(result);
+                }
+            }
+            int min = 1;
+            while (set.Contains(min))
+            {
+                min++;
+            }
+            return min;
+        }
+    }
+}
