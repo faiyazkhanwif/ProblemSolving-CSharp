227a765d99297cc5e8ec566e710ce5cd87a8c228
faiyazkhanwif <faiyazkhanwif@gmail.com>
2023-05-31 02:20:43 +0800

    Easy

---
+++ ProblemSolving-CSharp/LeetcodeTopInterview150/0027.RemoveElements.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp.LeetcodeTopInterview150
+{
+    internal class _0027
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int index = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[index] = nums[i];
+                    index++;
+                }
+            }
+            return index;
+        }
+
+    }
+}
---
+++ ProblemSolving-CSharp/LeetcodeTopInterview150/0088.MergeSortedArray.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp.LeetcodeTopInterview150
+{
+    internal class _0088
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int pnum1 = m - 1;
+            int pnum2 = n - 1;
+
+            int index = nums1.Length - 1;
+
+            while (pnum1 > -1 && pnum2 > -1)
+            {
+                if (nums1[pnum1] < nums2[pnum2])
+                {
+                    nums1[index] = nums2[pnum2];
+                    Console.WriteLine(nums1[index]);
+                    pnum2--;
+                }
+                else
+                {
+                    nums1[index] = nums1[pnum1];
+                    Console.WriteLine(nums1[index]);
+                    pnum1--;
+                }
+
+                index--;
+            }
+
+            if (index > -1)
+            {
+                //Console.WriteLine("LeftOut");
+                while (pnum1 > -1)
+                {
+                    nums1[index] = nums1[pnum1];
+                    Console.WriteLine(nums1[index]);
+                    pnum1--;
+                    index--;
+                }
+                while (pnum2 > -1)
+                {
+                    nums1[index] = nums2[pnum2];
+                    Console.WriteLine(nums1[index]);
+                    pnum2--;
+                    index--;
+                }
+            }
+        }
+    }
+}
