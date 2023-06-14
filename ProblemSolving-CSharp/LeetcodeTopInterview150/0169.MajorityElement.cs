165955c5244d85f2fc1c282522a7bb0e5b588a00
faiyazkhanwif <faiyazkhanwif@gmail.com>
2023-06-15 03:00:47 +0800

    Easy

---
+++ ProblemSolving-CSharp/LeetcodeTopInterview150/0169.MajorityElement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp.LeetcodeTopInterview150
+{
+    internal class _0169
+    {
+        public int MajorityElement(int[] nums)
+        {
+            int countToBeMatched = nums.Length / 2;
+            int index = 0;
+
+            Dictionary<int, int> numsWithCounts = new Dictionary<int, int>();
+
+            while (index < nums.Length)
+            {
+                if (numsWithCounts.ContainsKey(nums[index]))
+                {
+                    numsWithCounts[nums[index]]++;
+                    if (numsWithCounts[nums[index]] > countToBeMatched)
+                    {
+                        return nums[index];
+                    }
+                }
+                else
+                {
+                    numsWithCounts.Add(nums[index], 1);
+                }
+                index++;
+            }
+
+            return nums[0];
+
+        }
+    }
+
+}
