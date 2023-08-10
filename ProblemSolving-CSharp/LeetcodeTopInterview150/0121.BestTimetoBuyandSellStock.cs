26b23bb70526970095e7b05efb1d21da1eb80bac
faiyazkhanwif <faiyazkhanwif@gmail.com>
2023-08-10 11:57:31 +0800

    Easy

---
+++ ProblemSolving-CSharp/LeetcodeTopInterview150/0121.BestTimetoBuyandSellStock.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp.LeetcodeTopInterview150
+{
+    internal class _0121
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int maxProfit = 0;
+
+            int buyingDayIndex = 0;
+            int sellingDayIndex = 1;
+
+            while (buyingDayIndex<sellingDayIndex && buyingDayIndex<prices.Length)
+            {
+                if (sellingDayIndex >= prices.Length)
+                {
+                    return maxProfit;
+                }
+
+                if (prices[buyingDayIndex] > prices[sellingDayIndex])
+                {
+                    buyingDayIndex = sellingDayIndex;
+                }
+                else
+                {
+                    int profit = prices[sellingDayIndex] - prices[buyingDayIndex];
+
+                    if (profit > maxProfit)
+                    {
+                        maxProfit = profit;
+                    }
+
+                } 
+
+                sellingDayIndex++;
+            }
+
+            return maxProfit;
+        }
+    }
+}
---
+++ ProblemSolving-CSharp/LeetcodeTopInterview150/0122.BestTimetoBuyandSellStockII.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp.LeetcodeTopInterview150
+{
+    internal class _0122
+    {
+        public int MaxProfit(int[] prices)
+        {
+            return 0;
+        }
+    }
+}
