58c48409f324fa7f2de0bfbdac6f45f57f37a5f6
faiyazkhanwif <faiyazkhanwif@gmail.com>
2024-01-05 13:13:59 +0800

    Easy

---
+++ ProblemSolving-CSharp/2024/TopInterview150LC/0169.MajorityElement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ProblemSolving_CSharp._2024.TopInterview150LC
+{
+    internal class _0169
+    {
+        public int MajorityElement(int[] nums)
+        {
+            int n = nums.Length;
+            int element = 0;
+            Dictionary<int, int> elementCounts = new Dictionary<int, int>();
+            foreach (int i in nums)
+            {
+                if (elementCounts.ContainsKey(i))
+                {
+                    elementCounts[i]++;
+                }
+                else
+                {
+                    elementCounts[i] = 1;
+                }
+                if (elementCounts[i] > n / 2)
+                {
+                    element = i;
+                    break;
+                }
+            }
+            return element;
+        }
+    }
+}
