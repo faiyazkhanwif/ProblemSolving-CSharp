13d81d0e427df1ee5b34bfee747db39fe80afc5a
faiyazkhanwif <faiyazkhanwif@gmail.com>
2024-01-10 17:55:40 +0800

    Easy

---
+++ ProblemSolving-CSharp/2024/TopInterview150LC/0121.BestTimetoBuyAndSellStock.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_CSharp._2024.TopInterview150LC
+{
+    internal class _0121
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int maxProfit = 0;
+
+            if (prices.Length == 0)
+            {
+                return maxProfit;
+            }
+
+            int lowestPrice = prices[0];
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (lowestPrice > prices[i])
+                {
+                    lowestPrice = prices[i];
+                }
+                else
+                {
+                    int profit = prices[i] - lowestPrice;
+                    if (maxProfit < profit)
+                    {
+                        maxProfit = profit;
+                    }
+                }
+                //Console.WriteLine(maxProfit);
+            }
+
+            return maxProfit;
+        }
+    }
+}
